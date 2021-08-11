eb41ff4be5fa9c575e9277d8715d3338862caec2
AkhondSarah <akhond.sarah@gmail.com>
2021-08-11 00:05:32 -0400

    commit

---
+++ FinalProjectGame.cpp
@@ -0,0 +1,12 @@
+﻿// FinalProjectGame.cpp : Defines the entry point for the application.
+//
+
+#include "FinalProjectGame.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
