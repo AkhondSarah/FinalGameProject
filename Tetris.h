4450df96bf390c9040bcd6af10d303787281d6b1
Thoainhi Banh <nhibanh2407@gmail.com>
2021-08-11 16:13:09 -0400

    Update Tetris.h

--- Tetris.h
+++ Tetris.h
@@ -13,50 +13,43 @@ using namespace std;
 
 /*
 * Tetris class:
-* The original code was developed by Fam Trinliin 
+* The original code was developed by Fam Trinliin
 * his YouTube channel (see the following URL):  https://www.youtube.com/watch?v=zH_omFPqMO4&list=PLB_ibvUSN7mzUffhiay5g5GUHyJRO4DYr.
 * we use the concepts of electronic game  and competetive game here
 */
 
-// TETRIS gMAE - Source
+// TETRIS Game - Source
 
 class Tetris {
-
 public:
-	// SIze of the screen
+	// Size of the screen
 	static const int WIDTH = 480;
 	static const int HEIGHT = 480;
-
-	//POSITIONS (GRID)
+	//Positions (GRID)
 	static const int MAXLIN = 20;
 	static const int MAXCOL = 20;
-
-	//PICTURE REPRESENTAIION
+	//Picture representation
 	static const int DIM = 4;
-	// NUMBER OF GEOMETRIC BLOACKS
+	//Number of blocks
 	static const int NFIG = 18;
-	//NUMBER OF COLOURS
+	//Number of color
 	static const int NCOLOR = 16;
-	//sIZE OF THE PIXEL
+	//Pixel size
 	static const int PIX = 18;
-
-	//ARRAY OF POSSIBLE BLOCKA
+	//Array of blocks
 	const int BLOCKDIMX = 2;
 	const int BLOCKDIMY = 4;
 
 private:
-
-	
-
 	// List of figures
-	const int figures[NFIG][DIM] = 
-	{ 
+	const int figures[NFIG][DIM] =
+	{
 		0,0,0,0, // A single block
 		0,0,1,1, // 2 block side by side
 		0,0,2,2, // 2 blocks vertical
 		0,0,1,3, // 3 bloacks-2 blocks side by sdie a block below the secodn one it
-		0,0,2,3,// 3- mobcls- L shaped
-		0,0,2,4,//3 block vertical
+		0,0,2,3, // 3- mobcls- L shaped
+		0,0,2,4, //3 block vertical
 		1,1,0,2, //3 blocks 1,0,2
 		2,3,4,5, // 4 blocks
 		2,4,5,7, // 4-s kind of block shape
@@ -68,8 +61,7 @@ private:
 		2,4,6,7, // 4-L shaped  opposite
 		1,2,4,6, // 4 blocks 
 		1,2,5,7, // 4 blocks
-		7,4,2,0 // 4 blcoks(18)
-		
+		7,4,2,0 // 4 blcoks(18)	
 	};
 	// Points per block
 	const int points[NFIG] = {
@@ -95,71 +87,74 @@ private:
 		4,
 
 	};
-	struct Block { 
-		int x, y; 
+	struct Block {
+		int x, y;
 	}
 	posX[DIM] = { 0, 0, 0, 0 },
-	posY[DIM] = { 0, 0, 0, 0 };
-
+		posY[DIM] = { 0, 0, 0, 0 };
 
 public:
-
 	inline bool nextMovement(int space[MAXLIN][MAXCOL]) {
 		for (int i = 0; i < DIM; i++)
-			if (posX[i].x < 0 || posX[i].x >= MAXCOL ||
-				posX[i].y >= MAXLIN){ }
-				// TODO: Complete the return here
-			else if (space[posX[i].y][posX[i].x]){}
-				// TODO: Complete the return here
-				// TODO: Complete the return here
-
+			if (posX[i].x < 0 || posX[i].x >= MAXCOL || posX[i].y >= MAXLIN) {
+				return false;
+			}
+			else if (space[posX[i].y][posX[i].x]) {
+				return false;
+			}
+		return true;
 	};
-
 	void checksFill(int space[MAXLIN][MAXCOL]) {
 		int k = MAXLIN - 1;
 		for (int i = MAXLIN - 1; i > 0; i--) {
 			int count = 0;
 			for (int j = 0; j < MAXCOL; j++) {
-				if (space[i][j])
-					// TODO: Update the count here
-					space[k][j] = space[i][j];
+				if (space[i][j]) {
+					count++;
+				}
+				space[k][j] = space[i][j];
 			}
 			if (count < MAXCOL) {
-
+				k--;
 			}
-				// TODO: Update k here
 		}
 	}
 	void horizontalMovement(int space[MAXLIN][MAXCOL], int dx) {
 		for (int i = 0; i < DIM; i++) {
-			// TODO: Update posX and posY
+			posX[i] = posY[i];
+			posX[i].x += dx;
 		}
-		if (nextMovement(space))
-			for (int i = 0; i < DIM; i++)
-				// TODO: Update posX
-			{ }
+		if (nextMovement(space)) {
+			for (int i = 0; i < DIM; i++) {
+				posX[i] = posY[i];
+			}
+		}
 	}
-
 	void rotationalMovement(int space[MAXLIN][MAXCOL]) {
 		Block p = posX[1]; //Central position
 		for (int i = 0; i < DIM; i++) {
-			// TODO: Update posX
+			int x = posX[i].y - p.y;
+			int y = posX[i].x - p.x;
+			posX[i].x = p.x - x;
+			posX[i].y = p.y + y;
 		}
-		if (nextMovement(space))
-			for (int i = 0; i < DIM; i++)
-				// TODO: Update pos
-			{ }
+		if (nextMovement(space)) {
+			for (int i = 0; i < DIM; i++) {
+				posX[i] = posY[i];
+			}
+		}
 	}
 	void downMovement() {
 		for (int i = 0; i < DIM; i++) {
-			// TODO: Update posX and posY
+			posX[i].y += 1;
+			posY[i].y += 1;
 		}
 	}
 	int createBlock(int space[MAXLIN][MAXCOL], int colorNum) {
 		int currentBlock;
 		// Updates all positions to create a new block
 		for (int i = 0; i < DIM; i++) {
-			// TODO: Updates the color of space[posy]posx]
+			space[posX[i].y][posX[i].x] = colorNum;
 		}
 		// TODO: Creates a new currentBlock randomly
 		return currentBlock;
@@ -169,7 +164,7 @@ public:
 			// TODO: Updates the posXusing the figures in the position given by currentBlock
 		}
 	}
-	inline int play( long currentTimeout, int currentTurn, Player player, int limTimeout) {
+	inline int play(long currentTimeout, int currentTurn, Player player, int limTimeout) {
 
 		// Game Model
 		int model[MAXLIN][MAXCOL] = { 0 };
